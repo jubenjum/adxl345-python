a3ebfb82c66c9775d9fc761f3fef188379727d37
Juan Benjumea <jubenjum@gmail.com>
2019-09-25 16:20:34 +0200

    refactored get_sensors_adxl345.py

--- get_sensor_adxl345.py
+++ get_sensor_adxl345.py
@@ -8,67 +8,80 @@
 # This code is designed to work with the ADXL345_I2CS I2C Mini Module available from ControlEverything.com.
 # https://www.controleverything.com/content/Accelorometer?sku=ADXL345_I2CS#tabs-0-product_tabset-2
 
-import smbus
+import signal
+import sys
 import time
 
-ADXL345_ADDRESS = 0x1D
+import smbus
+
+assert len(sys.argv) == 2, "missing output file"
+ofile = sys.argv[1]
+
+# handle ctr-c
+def signal_handler(sig, frame):
+    print('done')
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+
+sr = 100.0
+ADXL345_ADDRESS          = 0x53
+ADXL345_REG_DEVID        = 0x00 # Device ID
+ADXL345_REG_DATAX0       = 0x32 # X-axis data 0 (6 bytes for X/Y/Z)
+ADXL345_REG_POWER_CTL    = 0x2D # Power-saving features control
+ADXL345_REG_DATA_FORMAT  = 0x31
+ADXL345_REG_BW_RATE      = 0x2C
+ADXL345_DATARATE_0_10_HZ = 0x00
+ADXL345_DATARATE_0_20_HZ = 0x01
+ADXL345_DATARATE_0_39_HZ = 0x02
+ADXL345_DATARATE_0_78_HZ = 0x03
+ADXL345_DATARATE_1_56_HZ = 0x04
+ADXL345_DATARATE_3_13_HZ = 0x05
+ADXL345_DATARATE_6_25HZ  = 0x06
+ADXL345_DATARATE_12_5_HZ = 0x07
+ADXL345_DATARATE_25_HZ   = 0x08
+ADXL345_DATARATE_50_HZ   = 0x09
+ADXL345_DATARATE_100_HZ  = 0x0A # (default)
+ADXL345_DATARATE_200_HZ  = 0x0B
+ADXL345_DATARATE_400_HZ  = 0x0C
+ADXL345_DATARATE_800_HZ  = 0x0D
+ADXL345_DATARATE_1600_HZ = 0x0E
+ADXL345_DATARATE_3200_HZ = 0x0F
+ADXL345_RANGE_2_G        = 0x00 # +/-  2g (default)
+ADXL345_RANGE_4_G        = 0x01 # +/-  4g
+ADXL345_RANGE_8_G        = 0x02 # +/-  8g
+ADXL345_RANGE_16_G       = 0x03 # +/- 16g
+
+
 
 # Get I2C bus
 bus = smbus.SMBus(1)
 
-# ADXL345 address, ADXL345_ADDRESS(83)
-# Select bandwidth rate register, 0x2C(44)
-#		0x0A(10)	Normal mode, Output data rate = 100 Hz
-bus.write_byte_data(ADXL345_ADDRESS, 0x2C, 0x0A)
-# ADXL345 address, ADXL345_ADDRESS(83)
-# Select power control register, 0x2D(45)
-#		0x08(08)	Auto Sleep disable
+# condiguring the adxl345
+bus.write_byte_data(ADXL345_ADDRESS, ADXL345_REG_BW_RATE , ADXL345_DATARATE_100_HZ)
 bus.write_byte_data(ADXL345_ADDRESS, 0x2D, 0x08)
-# ADXL345 address, ADXL345_ADDRESS(83)
-# Select data format register, 0x31(49)
-#		0x08(08)	Self test disabled, 4-wire interface
-#					Full resolution, Range = +/-2g
-bus.write_byte_data(ADXL345_ADDRESS, 0x31, 0x08)
+bus.write_byte_data(ADXL345_ADDRESS, ADXL345_REG_DATA_FORMAT, ADXL345_RANGE_2_G)
 
-time.sleep(0.5)
+with open(ofile, "w") as f:
+    while 1:
 
-# ADXL345 address, ADXL345_ADDRESS(83)
-# Read data back from 0x32(50), 2 bytes
-# X-Axis LSB, X-Axis MSB
-data0 = bus.read_byte_data(ADXL345_ADDRESS, 0x32)
-data1 = bus.read_byte_data(ADXL345_ADDRESS, 0x33)
+        initial_time = time.perf_counter()
+        data = bus.read_i2c_block_data(ADXL345_ADDRESS, 0x32, 6)
+        X = ((data[0] & 0x03) * 256) + data[1]
+        Y = ((data[2] & 0x03) * 256) + data[3]
+        Z = ((data[4] & 0x03) * 256) + data[5]
 
-# Convert the data to 10-bits
-xAccl = ((data1 & 0x03) * 256) + data0
-if xAccl > 511 :
-	xAccl -= 1024
+        if X > 511 :
+        	X -= 1024
 
-# ADXL345 address, ADXL345_ADDRESS(83)
-# Read data back from 0x34(52), 2 bytes
-# Y-Axis LSB, Y-Axis MSB
-data0 = bus.read_byte_data(ADXL345_ADDRESS, 0x34)
-data1 = bus.read_byte_data(ADXL345_ADDRESS, 0x35)
+        if Y > 511 :
+        	Y -= 1024
 
-# Convert the data to 10-bits
-yAccl = ((data1 & 0x03) * 256) + data0
-if yAccl > 511 :
-	yAccl -= 1024
+        if Z > 511 :
+        	Z -= 1024
 
-# ADXL345 address, ADXL345_ADDRESS(83)
-# Read data back from 0x36(54), 2 bytes
-# Z-Axis LSB, Z-Axis MSB
-data0 = bus.read_byte_data(ADXL345_ADDRESS, 0x36)
-data1 = bus.read_byte_data(ADXL345_ADDRESS, 0x37)
-
-# Convert the data to 10-bits
-zAccl = ((data1 & 0x03) * 256) + data0
-if zAccl > 511 :
-	zAccl -= 1024
-
-# Output data to screen
-print("Acceleration in X-Axis : {}".format(xAccl))
-print("Acceleration in Y-Axis : {}".format(yAccl))
-print("Acceleration in Z-Axis : {}".format(zAccl))
-
-
+        f.write("{} {} {} {}\n".format(initial_time, X, Y, Z))
+        ending_time = time.perf_counter()
+        sleep_time = 1.0/sr - (ending_time - initial_time) if 1.0/sr > (ending_time - initial_time) else 0.0
+        time.sleep(sleep_time)
 
